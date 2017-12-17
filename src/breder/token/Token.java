beb2d74bf8991a6c214c035b945b50c11de3ce2d
Bernardo Breder <bernardobreder@gmail.com>
2017-12-17 09:41:35 -0200

    .

--- src/breder/token/Token.java
+++ src/breder/token/Token.java
@@ -71,6 +71,51 @@ public class Token {
    * {@inheritDoc}
    */
   @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + offset;
+    result = prime * result + length;
+    result = prime * result + ((source == null) ? 0 : source.hashCode());
+    return result;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Token other = (Token) obj;
+    if (offset != other.offset) {
+      return false;
+    }
+    if (length != other.length) {
+      return false;
+    }
+    if (source == null) {
+      if (other.source != null) {
+        return false;
+      }
+    }
+    else if (!source.equals(other.source)) {
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
   public String toString() {
     return word;
   }
