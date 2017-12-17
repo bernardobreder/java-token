3ade9f9a91f171b152446cbd501d5c5290d007ba
berna <berna@DESKTOP-MBLT235>
2017-12-17 19:18:53 -0200

    .

--- src/breder/token/Grammar.java
+++ src/breder/token/Grammar.java
@@ -4,200 +4,199 @@ import java.text.ParseException;
 
 public class Grammar {
 
-  private final Token[] tokens;
+	private final Token[] tokens;
 
-  private int index;
+	private int index;
 
-  public Grammar(Token[] tokens) {
-    this.tokens = tokens;
-    this.index = 0;
-  }
+	public Grammar(Token[] tokens) {
+		this.tokens = tokens;
+		this.index = 0;
+	}
 
-  protected boolean eof() {
-    return index >= tokens.length;
-  }
+	protected boolean eof() {
+		return index >= tokens.length;
+	}
 
-  protected boolean eof(int count) {
-    return index + count >= tokens.length;
-  }
+	protected boolean eof(int count) {
+		return index + count >= tokens.length;
+	}
 
-  protected Token read(String word, String message) throws SyntaxException {
-    if (eof()) {
-      throw new SyntaxException(last(), "expected '" + word
-        + "' but was found <EOF> after token");
-    }
-    Token token = token();
-    if (token.is(word)) {
-      index++;
-      return token;
-    }
-    throw new SyntaxException(token, message);
-  }
+	protected Token read(String word, String message) throws SyntaxException {
+		if (eof()) {
+			throw new SyntaxException(last(), "expected '" + word + "' but was found <EOF> after token");
+		}
+		Token token = token();
+		if (token.is(word)) {
+			index++;
+			return token;
+		}
+		throw new SyntaxException(token, message);
+	}
 
-  protected Token read(char word, String message) throws SyntaxException {
-    if (eof()) {
-      throw new SyntaxException(last(), "expected <" + word
-        + "> but was found <EOF> after token");
-    }
-    Token token = token();
-    if (token.is(word)) {
-      index++;
-      return token;
-    }
-    throw new SyntaxException(token, message);
-  }
+	protected Token read(char word, String message) throws SyntaxException {
+		if (eof()) {
+			throw new SyntaxException(last(), "expected <" + word + "> but was found <EOF> after token");
+		}
+		Token token = token();
+		if (token.is(word)) {
+			index++;
+			return token;
+		}
+		throw new SyntaxException(token, message);
+	}
 
-  protected Token readIdentifier(String message) throws SyntaxException {
-    if (eof()) {
-      throw new SyntaxException(last(),
-        "expected <id> but was found <EOF> after token");
-    }
-    Token token = token();
-    if (token.isIdentifier()) {
-      index++;
-      return token;
-    }
-    throw new SyntaxException(token, message);
-  }
+	protected Token readIdentifier(String message) throws SyntaxException {
+		if (eof()) {
+			throw new SyntaxException(last(), "expected <id> but was found <EOF> after token");
+		}
+		Token token = token();
+		if (token.isIdentifier()) {
+			index++;
+			return token;
+		}
+		throw new SyntaxException(token, message);
+	}
 
-  protected Token readNumber(String message) throws SyntaxException {
-    if (eof()) {
-      throw new SyntaxException(last(),
-        "expected <number> but was found <EOF> after token");
-    }
-    Token token = token();
-    if (token.isNumber()) {
-      index++;
-      return token;
-    }
-    throw new SyntaxException(token, message);
-  }
+	protected Token readNumber(String message) throws SyntaxException {
+		if (eof()) {
+			throw new SyntaxException(last(), "expected <number> but was found <EOF> after token");
+		}
+		Token token = token();
+		if (token.isNumber()) {
+			index++;
+			return token;
+		}
+		throw new SyntaxException(token, message);
+	}
 
-  protected Token readString(String message) throws SyntaxException {
-    if (eof()) {
-      throw new SyntaxException(last(),
-        "expected <string> but was found <EOF> after token");
-    }
-    Token token = token();
-    if (token.isString()) {
-      index++;
-      return token;
-    }
-    throw new SyntaxException(token, message);
-  }
+	protected Token readString(String message) throws SyntaxException {
+		if (eof()) {
+			throw new SyntaxException(last(), "expected <string> but was found <EOF> after token");
+		}
+		Token token = token();
+		if (token.isString()) {
+			index++;
+			return token;
+		}
+		throw new SyntaxException(token, message);
+	}
 
-  protected boolean can(String word) {
-    if (!eof() && token().is(word)) {
-      index++;
-      return true;
-    }
-    return false;
-  }
+	protected boolean can(String word) {
+		if (!eof() && token().is(word)) {
+			index++;
+			return true;
+		}
+		return false;
+	}
 
-  protected boolean can(char word) {
-    if (!eof() && token().is(word)) {
-      index++;
-      return true;
-    }
-    return false;
-  }
+	protected boolean can(char word) {
+		if (!eof() && token().is(word)) {
+			index++;
+			return true;
+		}
+		return false;
+	}
 
-  protected boolean can(char word1, char word2) {
-    if (!eof(1) && token().is(word1) && token(1).is(word2)) {
-      index += 2;
-      return true;
-    }
-    return false;
-  }
+	protected boolean can(char word1, char word2) {
+		if (!eof(1) && token().is(word1) && token(1).is(word2)) {
+			index += 2;
+			return true;
+		}
+		return false;
+	}
 
-  protected boolean is(String word) {
-    return !eof() && token().is(word);
-  }
+	protected boolean is(String word) {
+		return !eof() && token().is(word);
+	}
 
-  protected boolean is(int next, String word) {
-    return !eof(next) && token(next).is(word);
-  }
+	protected boolean is(int next, String word) {
+		return !eof(next) && token(next).is(word);
+	}
 
-  protected boolean is(char word) {
-    return !eof() && token().is(word);
-  }
+	protected boolean is(char word) {
+		return !eof() && token().is(word);
+	}
 
-  protected boolean is(int next, char word) {
-    return !eof(next) && token(next).is(word);
-  }
+	protected boolean is(int next, char word) {
+		return !eof(next) && token(next).is(word);
+	}
 
-  protected boolean isNumber() {
-    return !eof() && token().isNumber();
-  }
+	protected boolean is(char char1, char char2) {
+		return is(char1) && is(1, char2);
+	}
 
-  protected boolean isString() {
-    return !eof() && token().isString();
-  }
+	protected boolean is(String str1, String str2) {
+		return is(str1) && is(1, str2);
+	}
 
-  protected boolean isIdentifier() {
-    return !eof() && token().isIdentifier();
-  }
+	protected boolean isNumber() {
+		return !eof() && token().isNumber();
+	}
 
-  protected boolean isNumber(int next) {
-    return !eof(next) && token(next).isNumber();
-  }
+	protected boolean isString() {
+		return !eof() && token().isString();
+	}
 
-  protected boolean isString(int next) {
-    return !eof(next) && token(next).isString();
-  }
+	protected boolean isIdentifier() {
+		return !eof() && token().isIdentifier();
+	}
 
-  protected boolean isIdentifier(int next) {
-    return !eof(next) && token(next).isIdentifier();
-  }
+	protected boolean isNumber(int next) {
+		return !eof(next) && token(next).isNumber();
+	}
 
-  protected Token next() {
-    return tokens[index++];
-  }
+	protected boolean isString(int next) {
+		return !eof(next) && token(next).isString();
+	}
 
-  protected Token next(int count) {
-    Token begin = token();
-    index += count - 1;
-    Token end = token();
-    index++;
-    return begin.join(end);
-  }
+	protected boolean isIdentifier(int next) {
+		return !eof(next) && token(next).isIdentifier();
+	}
 
-  protected Token token() {
-    return tokens[index];
-  }
+	protected Token next() {
+		return tokens[index++];
+	}
 
-  protected Token token(int next) {
-    return tokens[index + next];
-  }
+	protected Token next(int count) {
+		Token begin = token();
+		index += count - 1;
+		Token end = token();
+		index++;
+		return begin.join(end);
+	}
 
-  protected Token last() {
-    return tokens[tokens.length - 1];
-  }
+	protected Token token() {
+		return tokens[index];
+	}
 
-  protected SyntaxException error(String message) {
-    if (eof()) {
-      Token token = last();
-      return new SyntaxException(token, "[" + token.word + "," + token.offset
-        + "] " + message);
-    }
-    else {
-      Token token = token();
-      return new SyntaxException(token, "[" + token.word + "," + token.offset
-        + "] " + message);
-    }
-  }
+	protected Token token(int next) {
+		return tokens[index + next];
+	}
 
-  public static class SyntaxException extends ParseException {
+	protected Token last() {
+		return tokens[tokens.length - 1];
+	}
 
-    public final Token token;
+	protected SyntaxException error(String message) {
+		if (eof()) {
+			Token token = last();
+			return new SyntaxException(token, "[" + token.word + "," + token.offset + "] " + message);
+		} else {
+			Token token = token();
+			return new SyntaxException(token, "[" + token.word + "," + token.offset + "] " + message);
+		}
+	}
 
-    public SyntaxException(Token token, String message) {
-      super(String.format("at %s with token '%s' at line %d and column %d: %s",
-        token.source, token.word, token.line, token.column, message),
-        token.offset);
-      this.token = token;
-    }
+	public static class SyntaxException extends ParseException {
 
-  }
+		public final Token token;
+
+		public SyntaxException(Token token, String message) {
+			super(String.format("at %s with token '%s' at line %d and column %d: %s", token.source, token.word,
+					token.line, token.column, message), token.offset);
+			this.token = token;
+		}
+
+	}
 
 }
