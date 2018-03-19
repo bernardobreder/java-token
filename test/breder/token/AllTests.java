1e87b6e4f463131a7718b23d6bc29d598980e77b
Bernardo Breder <bernardobreder@gmail.com>
2018-03-18 22:20:16 -0300

    .

---
+++ test/breder/token/AllTests.java
@@ -0,0 +1,14 @@
+package breder.token;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ //
+    LexerTest.class, //
+    TokenizerTest.class, //
+})
+public class AllTests {
+
+}
